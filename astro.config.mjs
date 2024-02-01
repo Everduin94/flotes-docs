9c85253bc018cff4c381334018af6afdce9d262e
everduin94 <everduin94@gmail.com>
2024-02-01 11:34:00 -0600

    Add tailwind import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
