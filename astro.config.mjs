509cafa476ff8c573292677e560df396264f3314
everduin94 <everduin94@gmail.com>
2024-02-13 15:10:36 -0600

    ✨ feat: FLO-35 pro pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,53 +1,55 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Flotes Documentation',
-      customCss: [
-        './src/tailwind.css'
-      ],
+  integrations: [
+    starlight({
+      title: "Flotes Documentation",
+      customCss: ["./src/tailwind.css"],
       logo: {
         replacesTitle: true,
-        dark: './src/assets/logo-dark-1.svg',
-        light: './src/assets/logo-light-1.svg',
+        dark: "./src/assets/logo-dark-1.svg",
+        light: "./src/assets/logo-light-1.svg",
       },
-			social: {
-				github: 'https://github.com/everduin94',
-        discord: 'https://discord.gg/qKaKeGT8sZ'
-			},
-			sidebar: [
+      social: {
+        github: "https://github.com/everduin94",
+        discord: "https://discord.gg/qKaKeGT8sZ",
+      },
+      sidebar: [
         {
-					label: 'Getting Started',
-          autogenerate: { directory: 'getting-started' }
-				},
-				{
-					label: 'Guides',
+          label: "Getting Started",
+          autogenerate: { directory: "getting-started" },
+        },
+        {
+          label: "Guides",
           items: [
-            { label: 'Notebooks', link: '/guides/notebooks'},
-            { label: 'Notes', link: '/guides/notes'},
-            { label: 'Study', link: '/guides/study'},
-            { label: 'Stats', link: '/guides/stats'},
-            { label: 'Spaced Repetition', link: '/guides/spaced-repetition'},
-            { label: 'Archives', link: '/guides/archives'},
-            { label: 'Sharing', link: '/guides/sharing'},
-            { label: 'Tags', link: '/guides/tags'},
-            { label: 'Resources', link: '/guides/resources'},
-	    { label: 'Webhooks', link: '/guides/webhooks'},
-          ]
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+            { label: "Notebooks", link: "/guides/notebooks" },
+            { label: "Notes", link: "/guides/notes" },
+            { label: "Study", link: "/guides/study" },
+            { label: "Stats", link: "/guides/stats" },
+            { label: "Spaced Repetition", link: "/guides/spaced-repetition" },
+            { label: "Archives", link: "/guides/archives" },
+            { label: "Sharing", link: "/guides/sharing" },
+            { label: "Tags", link: "/guides/tags" },
+            { label: "Resources", link: "/guides/resources" },
+            { label: "Webhooks", link: "/guides/webhooks" },
+          ],
+        },
+        {
+          label: "Pro",
+          items: [{ label: "Intro", link: "/pro/intro" }],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
     tailwind({
       // Disable the default base styles:
       applyBaseStyles: false,
     }),
-	],
+  ],
 });
---
+++ src/content/docs/pro/intro.md
@@ -0,0 +1,50 @@
+---
+title: Pro Features
+description: A guide to Flotes pro features
+---
+
+
+## Webhooks
+
+> *Requires some setup*
+
+Integrate Flotes with 100s of other apps like: **Trello, Notion, Google Calendar, G-Mail, Todoist**
+- Create todos out of future study sessions
+- Automatically schedule the optimal study date in other apps
+- Upload notes to other note-taking or storage apps after archiving a notebook
+
+![Flotes Waiting](../../../assets/flotes-waiting.png)
+
+[See guides & examples](https://docs.flotes.app/guides/webhooks)
+
+
+## Enhanced Feedback
+
+> *Does not require setup*
+
+Flotes pro will track feedback and object measurments during study sessions **on a per-note basis**. This unlocks many potential features to improve your notes
+- Identify problem areas
+- Find strengths and weaknesses
+- Split or simplify where too difficult
+- Aggregate or add challenge where too easy
+
+In this example, Data Structures and Algorithm Computer Science notes have been highlighted with their current status from the last study session.
+
+![feedback](https://ik.imagekit.io/flotes/2024-02-12_17-00-EDIT-EDIT.jpg?updatedAt=1707779675265)
+
+A more advanced example calculates the status from multiple study session or objective measures. Advanced statuses like this can help identify problem areas or room for improvement
+- **Very easy** identifies areas to increase the challenge or combine multiple very easy into a single more challenging note
+- **Leech** can identify notes that are taking significant amounts of time to study and may be difficult to understand
+- **Stagnating** highlights notes that seem medium difficulty, but are deceptively difficult, because they aren't getting easier with practice.
+
+![feedback-2](https://ik.imagekit.io/flotes/notes-color-2-EDIT.jpg?updatedAt=1707085806437)
+
+:::note[Future Features]
+
+We plan to extend this functionality into other parts of the app.
+
+:::
+
+
+
+
